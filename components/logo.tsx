9c436386185054f2ed2aa3b7df80d8b090c3233f
Muzi Phage <muzi.phage@silvabak.co.za>
2025-05-31 13:04:34 +0200

    first commit

---
+++ components/logo.tsx
@@ -0,0 +1,12 @@
+import  Image  from 'next/image';
+import logopic from '../public/icon0.svg';
+
+export default function Logo() {
+    return(
+        <Image
+            src={logopic}
+            alt="Silvabak Cloud Tech Logo"
+            className="h-7 w-auto"
+        />
+    );
+}
