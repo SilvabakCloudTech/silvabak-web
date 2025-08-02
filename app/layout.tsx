28a514d9150928f070297ac360bf6888e37fca5e
Muzi Phage <muzi.phage@silvabak.co.za>
2025-08-02 10:04:49 +0200

    Fixed footer link

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,20 +38,32 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-title" content="Silvabak Cloud Tech" />
       </head>
       <body
-        className= {clsx(
+        className={clsx(
           "min-h-screen text-foreground bg-background font-sans antialiased",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "system", enableSystem: true }}>
+        <Providers
+          themeProps={{
+            attribute: "class",
+            defaultTheme: "system",
+            enableSystem: true,
+          }}
+        >
           <div className="dark:bg-slate-950 relative flex flex-col h-screen">
             <Navbar />
             <main className="container mx-auto max-w-7xl flex-grow">
               {children}
             </main>
             <footer className="w-full flex items-center justify-center py-3">
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">Silvabak Cloud Tech</p>
+              <Link
+                isExternal
+                className="flex items-center gap-1 text-current"
+                href="https://www.silvabak.co.za"
+                title="silvabak.co.za homepage"
+              ></Link>
+              <span className="text-default-600">Powered by </span>
+              <p className="text-primary">Silvabak Cloud Tech</p>
             </footer>
           </div>
         </Providers>
